ad3d1834dc830ac9a474a760dfad8856549df7ec
Willi Woldt <willi.woldt@ueberbit.de>
2023-05-02 22:08:54 +0000

    update devcontainer

--- .ddev/pre-start.sh
+++ .ddev/pre-start.sh
@@ -1,5 +1,6 @@
 NODE_VERSION=$(cat .node-version)
 echo "nodejs_version: \"${NODE_VERSION}\"" > .ddev/config.node.yaml
+echo $NODE_VERSION > .nvmrc
 
 PHP_VERSION=$(cat .php-version)
 echo "php_version: \"${PHP_VERSION}\"" > .ddev/config.php.yaml
